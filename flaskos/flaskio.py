11fc460c6029282f1c6fa6971cbd8c69e5c40680
princegeorgio <gerorge.oblivantsev8@gmail.com>
2023-11-27 08:07:04 +0300

    article

--- flaskos/flaskio.py
+++ flaskos/flaskio.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, url_for, request, flash, session, redirect, abort
 from  flaskos.flskDB import FlaskoDB
 
 DATABASE = 'flasko.db'
@@ -33,7 +33,32 @@ def get_db():
 def index():
     db = get_db()
     dbase = FlaskoDB(db)
-    return render_template("index.html", title="HomE", menu=dbase.get_menu())
+    return render_template("index.html", title="HomE", menu=dbase.get_menu(),posts=dbase.get_posts_anonce())
+
+@app.route("/add_post", methods=["POST", "GET"])
+def add_post():
+    db = get_db()
+    dbase = FlaskoDB(db)
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash("Ошибка добавления статьи!", category='error')
+            else: flash("Статья добавлена успешно!", category='success')
+        else:
+            flash("Ошибка добавления статьи!", category='error')
+
+    return render_template("add_post.html", title="Add Article", menu=dbase.get_menu())
+
+@app.route("/posts/<alias>")
+def show_post(alias):
+    db = get_db()
+    dbase = FlaskoDB(db)
+    title, post = dbase.get_post(alias)
+    if not title:
+        abort(404)
+
+    return render_template("post.html", menu=dbase.get_menu(), title=title, post=post)
 
 @app.route("/about")
 def about():
--- flaskos/flskDB.py
+++ flaskos/flskDB.py
@@ -1,3 +1,9 @@
+import sqlite3
+import time
+import math
+import re
+from flask import url_for
+
 class FlaskoDB:
     def __init__(self, db):
         self.__db = db
@@ -8,8 +14,48 @@ class FlaskoDB:
         try:
             self.__cur.execute(sql)
             res = self.__cur.fetchall()
+            return res if res else []
+        except sqlite3.Error as e:
+            print("Ошибка чтения из БД" + str(e))
+            return []
+
+    def add_post(self, title, text, url):
+        try:
+            self.__cur.execute("SELECT COUNT() as 'count' FROM post WHERE url LIKE ?",(url,))
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("Статья с таким url уже существует")
+                return False
+
+            base = url_for('static', filename="images")
+            text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>", r"\g<tag>" + base + r"/\g<url>>", text)
+
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO post VALUES(NULL, ?, ?, ?, ?)", (title, text, url, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Ошибка добавления статьи в БД" + str(e))
+            return False
+        return True
+
+    def get_post(self, alias):
+        try:
+            self.__cur.execute(f"SELECT title, text FROM post WHERE url LIKE '{alias}'")
+            res = self.__cur.fetchone()
             if res:
                 return res
-        except IOError:
-            print("Ошибка чтения из БД")
-            return
+        except sqlite3.Error as e:
+            print("Ошибка добавления статьи в БД" + str(e))
+
+            return False, False
+
+    def get_posts_anonce(self):
+        try:
+            self.__cur.execute(f"SELECT id, title, text, url FROM post ORDER BY time DESC")
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Ошибка добавления статьи в БД" + str(e))
+
+        return []
